8ac88b9a4b496fa3324a112f1ae82a7404fa8b33
Henrik Fricke <henrik.fricke@jimdo.com>
2019-09-04 18:32:02 +0200

    Export handler in function

--- functions/helloworld.js
+++ functions/helloworld.js
@@ -1,5 +1,8 @@
-export function(event, context, callback) {
-    const {identity, user} = context.clientContext;
-    console.log("Hello", identity, user);
-    callback();
-  }
+exports.handler = function(event, context, callback) {
+  console.log("Event", event);
+
+  const { identity, user } = context.clientContext;
+  console.log("Hello", identity, user);
+
+  callback();
+};
